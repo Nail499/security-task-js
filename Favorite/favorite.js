62ff4ba412f153e3e64e92e018b0a1eaaa7c87b5
Nail499 <ibadovv079@gmail.com>
2023-12-14 16:53:33 +0400

    16:53

--- Favorite/favorite.js
+++ Favorite/favorite.js
@@ -1,19 +1,5 @@
-const navbar = document.querySelector(".navbar")
-
-window.onscroll =scrollFunction
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        navbar.classList.add("navscroll")
-    } else {
-        navbar.classList.remove("navscroll")
-    }
-}
-
-
-
-let s2boxes = document.getElementById("s2boxes");
-fetch("http://localhost:3000/services")
+let s2boxes = document.getElementById("favorites");
+fetch("http://localhost:3000/favorites")
 .then(response=>response.json())
 .then(data=>{
     data.forEach(item=>{
@@ -34,22 +20,11 @@ fetch("http://localhost:3000/services")
     })
 })
 
-
 const deleteItem = (id) => {
-    axios.delete('http://localhost:3000/services/'+id)
+    axios.delete('http://localhost:3000/favorites/'+id)
     .then(res=>{
       console.log(res.data);
       alert(`${id} nömrəli  uğurla silindi`)
       window.location.reload();
     })
-  }
-
-  function addFavorite(id) {
-    axios.get('http://localhost:3000/services/'+id)
-    .then(res=>{
-        
-        axios.post('http://localhost:3000/favorites',res.data)
-       
-    })
-    
   }
